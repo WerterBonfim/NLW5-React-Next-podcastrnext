033e2dc4c9f8d42dbbefbb1f8a5cdd7471cc1d99
Werter Bonfim <werter@hotmail.com.br>
2021-04-23 22:35:23 -0300

    finalizando a aula 3

---
+++ .docs/observacoes.md
@@ -0,0 +1,50 @@
+# SSR - Server Side Render
+
+* O next consome os recursos (API) a cada requisição, busca os dados
+atrávez da função getServerSideProps
+
+```typescript
+
+// SSR
+export default function Home(props) {
+
+    // acesso ao props.episodes
+
+    return (
+
+    );
+}
+// Maneira do next efetuar um SSR
+export async function getServerSideProps() {
+    const response = await fetch('')
+    const data = await response.json();
+
+    return {
+        props: {
+            episodes: data
+        }
+    }
+}
+
+```
+# SSG - Server Static Generation
+ 
+* Gera um pagina estatica, desta forma o next não ira consumir recursos (API)
+a cada requisição, os dados ficam em cache pelo tempo que você configurou.
+
+
+```typescript
+
+// Maneira do next efetuar um SSG
+export async function getStaticProps() {
+    const response = await fetch('')
+    const data = await response.json();
+
+    return {
+        props: {
+            episodes: data
+        },
+        revalidate: 60 * 60 * 8 // a cada 8 horas sera gerado uma nova versão desta pagina
+    }
+}
+```
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,75 @@
-import { useEffect } from "react"
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import { api } from "../services/api";
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 
+type File = {
+  url: string;
+  type: string;
+  duration: number
+}
 
-export default function Home() {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  published_at: string;
+  description: string;
+  file: File;
+}
 
-  useEffect(() => {
-    
-  }, []);
+type HomeProps = {
+  episodes: Episode[]
+}
+
+
+export default function Home(props: HomeProps) {
 
   return (
-    <h1>Olá</h1>
+    <div>
+      <h1>Pagina inicial</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
+
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const { data } = await api.get<Episode[]>('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishAt: format(parseISO(episode.published_at), 'd MM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  })
+
+
+
+  return {
+    props: {
+      episodes: episodes
+    },
+    //revalidate: 60 * 60 * 8
+  }
+
+
+} 
